f7c45dfe5fc39611c763e2684ea7d4d9115711ee
GemmaClucas <gemma.clucas@cornell.edu>
2022-07-20 16:32:53 -0400

    Changed elution buffer reservoir to USA scientific 22ml

--- Zymo_fecal_bead_extractions_12columns.py
+++ Zymo_fecal_bead_extractions_12columns.py
@@ -37,7 +37,7 @@ def run(ctx):
     tipracks = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot)
                 for slot in [2, 4, 5, 7, 9]]
     elute_plate = ctx.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', 3)
-    elute_buff = ctx.load_labware('nest_12_reservoir_15ml', 6)
+    elute_buff = ctx.load_labware('usascientific_12_reservoir_22ml', 6)
     waste_res = ctx.load_labware('nest_1_reservoir_195ml', 11)
 
     # pipettes
